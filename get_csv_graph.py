9c10298c00a5b9bca5ee8f9b1286254605a405ab
kchon <30653165+kurichon@users.noreply.github.com>
2023-10-06 05:09:52 +0900

    Update get_csv_graph.py

--- get_csv_graph.py
+++ get_csv_graph.py
@@ -1,16 +1,63 @@
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import re
+
+
+#optimize constants make constant.py
+#create representation per model
 
 
 
-filename =  "/data_sample.csv"
-df = pd.read_csv(os.getcwd() + filename)
-print(len(df))
 
-headers = list(df)
+
+
+file_list = []
+dfs = []
+#os.listdir('dir_path'): Return the list of files and directories in a specified directory path.
+
+hp_model = ["vgg11","vgg16","vgg19","googlenet","lenet","densenet40_k12","densenet100_k12","inception3","inception4","resnet44","resnet56","resnet110"]
+#filename =  "/data_sample.csv"
+#path = 'data/GTX1080_imagenet/'
+#path = 'data/GTX1080_cifar10/'
+path = 'data/TitanX_cifar10/'
+#path = 'data/GTX1080_cifar10/'
+#path = 'data/GTX1080_cifar10/'
+list_of_models = os.listdir(path)
+for models in hp_model:
+    r = re.compile(".*"+ models +".*")
+    
+    file_list = list(filter(r.match, list_of_models))
+
+    for csv in file_list:
+        df = pd.read_csv(path + csv)
+        dfs.append(df)
+#df = pd.read_csv(os.getcwd() + filename)
+#print(len(df))
+
+final_df = pd.concat(dfs,ignore_index=True)
+print(final_df)
+headers = list(final_df)
 num_of_headers = len(headers)
 num_of_chunks = 5
+
+for i in range(0,num_of_headers):
+    #chunks = pd.read_csv(os.getcwd() + ,chunksize=round(len(df)/num_of_chunks))
+    #for j in range(0,num_of_chunks):
+     #   df_graph = pd.DataFrame(next(chunks))
+    x = final_df.index
+    y = final_df.iloc[:,i]
+    #linewidth = 2
+    
+    plt.plot(x,y,linewidth=0.5)
+    plt.title(headers[i])
+        #plt.show() if you want to check smaller chunks
+    plt.show()
+
+
+
+
+
 
 
 
@@ -22,18 +69,3 @@ num_of_chunks = 5
 #data_2 = next(chunks)
 #data_3 = next(chunks)
 #data_4 = next(chunks)
-
-
-
-for i in range(0,num_of_headers):
-    chunks = pd.read_csv(os.getcwd() + filename,chunksize=round(len(df)/num_of_chunks))
-    for j in range(0,num_of_chunks):
-        df_graph = pd.DataFrame(next(chunks))
-        x = df_graph.index
-        y = df_graph.iloc[:,i]
-    #linewidth = 2
-    
-        plt.plot(x,y,linewidth=0.5)
-    plt.title(headers[i])
-        #plt.show() if you want to check smaller chunks
-    plt.show()
